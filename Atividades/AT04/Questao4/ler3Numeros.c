d70f5d6aa92dd099b24217a94b81549eb8e8f549
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2021-08-24 03:11:24 -0300

    AT04-questao4

--- Atividades/AT04/Questao4/ler3Numeros.c
+++ Atividades/AT04/Questao4/ler3Numeros.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 int ler3numeros(){
-    int treenumbers[3],i;
+    int threenumbers[3],i;
 
     for(i=0;i<3;i++){
         printf("digite o %d numero\n",i);
-        scanf("%d",&treenumbers[i]);
-        return treenumbers[i];
+        scanf("%d",&threenumbers[i]);
+        return threenumbers[i];
     }
 
 }
