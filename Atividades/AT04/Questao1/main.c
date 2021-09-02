27f80044fb3698119c61bd02c4460d58bc41338c
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2021-09-02 00:32:56 -0300

    AT04-questao1

--- Atividades/AT04/Questao1/main.c
+++ Atividades/AT04/Questao1/main.c
@@ -6,9 +6,9 @@ int main(){
     int numero2 = 0;
     int soma = 0;
 
-    printf("digite o primeiro numero ");
+    printf("digite o primeiro numero....:  ");
     scanf("%d",&numero1);
-    printf("digite o segundo numero ");
+    printf("digite o segundo numero....: ");
     scanf("%d",&numero2);
 
     soma = somar(numero1,numero2);
